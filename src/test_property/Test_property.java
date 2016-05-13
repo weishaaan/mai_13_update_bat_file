22c3f58b26c0742f624c47438ef8d360ee64c009
weishan <weishaaan@gmail.com>
2016-05-13 16:06:47 +0200

    update .properties file and .bat files

---
+++ src/test_property/Test_property.java
@@ -0,0 +1,70 @@
+package test_property;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.Properties;
+
+public class Test_property {
+
+	public static void main(String[] s) {
+            Test_property r = new Test_property();          
+            r.createProperties();        
+            String filepath = r.readProperties();
+            System.out.println(filepath);
+            r.runBatFile(filepath);
+        }
+        public String readProperties(){
+            Properties prop = new Properties();
+            try (InputStream in = new FileInputStream("baaaaat.properties")) 
+            {
+		prop.load(in);
+            }
+	    catch (IOException e) {
+		e.printStackTrace();
+	    }
+            catch (Exception e) {
+		e.printStackTrace();
+	    }
+            String path = prop.getProperty("text.bat");
+            return path;
+        }
+
+        public void createProperties(){
+            try (OutputStream out = new FileOutputStream("baaaaat.properties")) {
+		Properties properties = new Properties();
+		properties.setProperty("text.bat", "C:\\Users\\stagiaire\\Documents\\NetBeansProjects\\test_property\\text.bat");
+		properties.setProperty("article", "JavaProperties");
+		properties.setProperty("version", "1.0");
+		properties.setProperty("ide", "eclipse");
+		properties.store(out, "This is a sample for java properties");
+            } catch (IOException e) {
+		e.printStackTrace();
+            }
+        }
+        
+        public void runBatFile(String filepath){
+            try {
+                String line;
+                Process p = Runtime.getRuntime().exec(filepath);
+                BufferedReader bri = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+            while ((line = bri.readLine()) != null) {
+                System.out.println(line);
+            }
+            bri.close();
+            System.out.println("end of getInputStream.");
+            System.out.println();
+            p.waitFor();
+            System.out.println("Exited with error code "+p.exitValue());
+            }
+            catch (Exception err) {
+                err.printStackTrace();
+            }
+        }
+}
---
+++ text.bat
@@ -0,0 +1,19 @@
+@ECHO OFF
+echo this is a test.bat file ,if the system time is before midi, the exit_code will be 01,else it will be 02.
+
+
+
+::exit 20
+::set the errorlevel manually, if we add this sentence
+
+rem setlocal enableDelayedExpansion
+
+rem for /f %%a in (c:\list.cvs) do (
+
+rem set var=%%a
+
+rem echo !var!
+
+rem )
+
+rem endloca
